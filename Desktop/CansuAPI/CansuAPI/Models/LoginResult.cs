5d02d8dc083e03bab7b1fc2150d5eebc2b1167d6
cyeksan <yeksancansu@gmail.com>
2021-10-04 11:01:22 +0300

    initial commit

---
+++ Desktop/CansuAPI/CansuAPI/Models/LoginResult.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using UserDataAccess;
+
+namespace CansuAPI.Models
+{
+    public class LoginResult
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+        [System.Text.Json.Serialization.JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public User User { get; set; }
+    }
+}
